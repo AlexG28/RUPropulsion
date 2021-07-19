63f16b3df49395522bafa6402ae74260b58bbef7
Alex <alex.gyumushyan@gmail.com>
2021-07-18 22:48:59 -0400

    working on navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
 import { MenuItems} from './MenuItems';
-
+import './Navbar.css';
 /* TODO:
     - not sure about the icon, should probably remove it 
+    - Naming is really messed up
 */
 
-
+//https://www.youtube.com/watch?v=fL8cFqhTHwA
 class Navbar extends Component {
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({clicked: !this.setState.clicked})
+    } // https://css-tricks.com/hamburger-menu-with-a-side-of-react-hooks-and-styled-components/
+
     render() { 
         return(
             <nav className="NavbarItems">
-                <h1 className="navbar-logo">React</h1>
-                <div className="menu-icon">
+                <h1 className="navbar-text">Ryerson Propulsion Group</h1>
+                
+                <div className="icon">
                     <img src="https://img.icons8.com/color/48/000000/rocket--v1.png"/>
                 </div> 
+               
+                <div className="menu-bars" onClick={this.handleClick}>
+                    <i className={this.state.clicked}></i>
+                    <img src="https://img.icons8.com/material-outlined/24/000000/menu--v1.png"/>
+                </div>
+                
                 <ul>
                     {MenuItems.map((item, index) => {
                         return (
