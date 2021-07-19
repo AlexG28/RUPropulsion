9eda4c2a81d66b7b844b0e3847dae8045f69eb17
Alex <alex.gyumushyan@gmail.com>
2021-07-19 12:27:54 -0400

    added mobile css

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
 import { MenuItems} from './MenuItems';
 import './Navbar.css';
+
+import { HiOutlineMenu } from 'react-icons/hi'
 /* TODO:
     - not sure about the icon, should probably remove it 
     - Naming is really messed up
 */
 
-//https://www.youtube.com/watch?v=fL8cFqhTHwA
+// https://www.youtube.com/watch?v=fL8cFqhTHwA
+// https://css-tricks.com/hamburger-menu-with-a-side-of-react-hooks-and-styled-components/
+
 class Navbar extends Component {
     
-
-    // https://css-tricks.com/hamburger-menu-with-a-side-of-react-hooks-and-styled-components/
-
+    
+    
     render() { 
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-text">Ryerson Propulsion Group</h1>
                 
-                <div className="icon">
+                <div className="navbar-icon">
                     <img src="https://img.icons8.com/color/48/000000/rocket--v1.png"/>
                 </div> 
-               
                 
+                <div className="menu-bars">
+                    <HiOutlineMenu />
+                </div>
                 
                 <ul className='nav-menu'>
                     {MenuItems.map((item, index) => {
