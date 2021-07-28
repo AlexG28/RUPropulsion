43e67ef09e7227e9b2ea4a4f2b8c13b38798894d
Alex <alex.gyumushyan@gmail.com>
2021-07-28 17:03:00 -0400

    footer 2

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { AiOutlineInstagram, AiOutlineFacebook, AiOutlineLinkedin, AiOutlineTwitter } from 'react-icons/ai';
+import './Footer.css';
 
 export default function Footer() {
     return (
-        <div>
-            <h1> haha this is a footer 1</h1>
+        <div className='footer-panel'>
+            <div className="footer-socialIcons">
+                <a href='https://www.facebook.com/rupropulsion/' target='_blank' rel="noreferrer"> <AiOutlineFacebook /> </a>
+                <a href='https://www.instagram.com/rupropulsion/?hl=en' target='_blank' rel="noreferrer">  <AiOutlineInstagram />  </a>
+                <a href='https://www.linkedin.com/company/rupropulsion/mycompany/' target='_blank' rel="noreferrer"> <AiOutlineLinkedin /> </a>
+                <a href='https://www.instagram.com/rupropulsion/?hl=en' target='_blank' rel="noreferrer"> <AiOutlineTwitter /> </a>
+            </div>
         </div>
     )
 }
