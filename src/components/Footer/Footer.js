76b32a7b7c36086ccaf332a4a6f6100bf42bfa81
Alex <alex.gyumushyan@gmail.com>
2021-07-28 16:23:37 -0400

    footer 1

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div>
-            <h1> haha this is a footer </h1>
+            <h1> haha this is a footer 1</h1>
         </div>
     )
 }
