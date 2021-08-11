fdf27488551d756c151e6cd5ba6ea6c7ea6dcab5
Alex <alex.gyumushyan@gmail.com>
2021-08-11 10:29:51 -0400

    working on basics of the main pages

--- src/pages/Mission/Mission.js
+++ src/pages/Mission/Mission.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-function Mission () {
+export default function Mission () {
 
     return (
         <div className="mission">
-            <h1>Mission</h1>
+            <h1>Milestones</h1>
+            <h3>In the past we have: </h3>
+            <ul>
+                <li>Assembled a driven team of students.</li>
+                <li> Consulted with numerous professionals in the industry.</li>
+                <li>Applied systems engineering to real-world design.</li>
+                <li>Explored new uses for additive manufacturing.</li>
+                <li>Produced a rocket engine design, ready for implementation.</li>
+            </ul>
         </div>
     );
 }
-
-export default Mission;
