f3810c906c37311dec5e199ffd3e1cd5dae690b0
Alex <alex.gyumushyan@gmail.com>
2021-08-26 12:52:44 -0400

    added basic features to the home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,7 +10,19 @@ function Home () {
     return (
         <div className="home">
             
-            <h1 className="main-text"> Ad astra </h1>
+            <p className="main-text"> Ryerson <br></br>   Propulsion <br></br> Group </p>
+
+            <div className = 'summary'>
+                <p>Fueling our future</p>
+                <p>
+                    Ryerson Propulsion group is a <br></br>
+                    student deisng team aiming to design, build<br></br>
+                    and test a liquid bipropellant rocket engine.<br></br>
+                    RPG consits of members from every engineering <br></br>
+                    discipline working on the complex challenges of a <br></br>
+                    rocket engine.
+                </p>
+            </div>
             <p> @RyersonPropulsionGroup2021 </p>
         </div>
     );
